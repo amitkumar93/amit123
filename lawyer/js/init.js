32ac2189a7a5c004ce88aaeaa2197bdbe452d99d
Amit Kumar <amit.kumar@settlin.in>
2016-01-11 10:42:32 +0530

    Ui Improved

--- lawyer/js/init.js
+++ lawyer/js/init.js
@@ -1,15 +1,11 @@
-(function($){
-  $(function(){
+(function($) {
+  $(function()  {
+    $(".button-collapse ").sideNav();
+   }); // end of document ready
+     })(jQuery); // end of jQuery name space
 
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-
-$(document).ready(function(){
-     $('.slider').slider({full_width:true});
-   });
-   $('#textarea1').val('New Text');
-   
-     $('#textarea1').trigger('autoresize');
+$(document).ready(function() {
+  $(".slider").slider({full_width: true});
+ });
+ $("#textarea1").val("New Text");
+ $("#textarea1").trigger("autoresize");
